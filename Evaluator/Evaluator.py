d97d573c77f3603c701c5263a78f5e3283607d97
vzaigraev <vzaigraev@yandex-team.ru>
2020-05-25 02:19:49 +0300

    Add new algorithms

--- Evaluator/Evaluator.py
+++ Evaluator/Evaluator.py
@@ -1,4 +1,4 @@
-from skimage.feature import blob_log
+from skimage.feature import blob_log, blob_dog, blob_doh
 from math import sqrt
 import numpy as np
 from skimage.filters import gaussian
@@ -15,8 +15,34 @@ def evaluateLOG(grayImage):
     coordinates[:, 3] = log[:, 0] + log[:, 2]
     return coordinates
 
-def evaluateLOGbatch(grayImages):
+# Difference of Gaussian
+def evaluateDOG(grayImage):
+    log = blob_dog(grayImage, max_sigma=30, threshold=.1)
+    log[:, 2] = log[:, 2] * sqrt(2)
+    coordinates = np.empty((log.shape[0], 5))
+    coordinates[:, 4] = 1
+    coordinates[:, 0] = log[:, 1] - log[:, 2]
+    coordinates[:, 1] = log[:, 0] - log[:, 2]
+    coordinates[:, 2] = log[:, 1] + log[:, 2]
+    coordinates[:, 3] = log[:, 0] + log[:, 2]
+    return coordinates
+
+# Determinant of Hessian
+def evaluateDOH(grayImage):
+    log = blob_doh(grayImage, max_sigma=30, threshold=.01)
+    #log[:, 2] = log[:, 2] * sqrt(2)
+    coordinates = np.empty((log.shape[0], 5))
+    coordinates[:, 4] = 1
+    coordinates[:, 0] = log[:, 1] - log[:, 2]
+    coordinates[:, 1] = log[:, 0] - log[:, 2]
+    coordinates[:, 2] = log[:, 1] + log[:, 2]
+    coordinates[:, 3] = log[:, 0] + log[:, 2]
+    return coordinates
+
+
+
+def evaluatebatch(grayImages, f):
     result = []
     for i in range(len(grayImages)):
-        result.append(evaluateLOG(grayImages[i]))
+        result.append(f(grayImages[i]))
     return result
